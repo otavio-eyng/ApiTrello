f1574bb2f9c4aa3a5a28114432b320696e46d2e0
Vinicius Durante Bagio <viniciusduranteb@gmail.com>
2025-06-12 18:59:10 -0300

    fix erros

--- src/main/java/com/example/tasks/Model/Task.java
+++ src/main/java/com/example/tasks/Model/Task.java
@@ -1,18 +1,21 @@
 package com.example.tasks.Model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String description;
     private String status;
